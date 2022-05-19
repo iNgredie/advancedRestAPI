7e1267407cd7852d062270e8d433247bed478f29
Aleksandr Durnev <sandslash@bk.ru>
2022-05-19 22:33:24 +0400

    add posts handlers

--- internal/posts/handler.go
+++ internal/posts/handler.go
@@ -11,6 +11,7 @@ var _ handlers.Handler = &handler{}
 
 const (
 	postsURL = "/posts"
+	postURL  = "/posts:uuid"
 )
 
 type handler struct {
@@ -25,8 +26,33 @@ func NewPostsHandler(logger *logging.Logger) handlers.Handler {
 
 func (h *handler) Register(router *gin.Engine) {
 	router.GET(postsURL, h.GetPostsList)
+	router.POST(postURL, h.CreatePost)
+	router.GET(postURL, h.GetPostByUUID)
+	router.PUT(postURL, h.UpdatePost)
+	router.PATCH(postURL, h.PartiallyUpdatePost)
+	router.DELETE(postURL, h.DeletePost)
 }
 
 func (h *handler) GetPostsList(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, "this is list of posts")
+}
+
+func (h *handler) CreatePost(c *gin.Context) {
+	c.IndentedJSON(http.StatusCreated, "this is create post")
+}
+
+func (h *handler) GetPostByUUID(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "this is post by uuid")
+}
+
+func (h *handler) UpdatePost(c *gin.Context) {
+	c.IndentedJSON(http.StatusNoContent, "this is update post")
+}
+
+func (h *handler) PartiallyUpdatePost(c *gin.Context) {
+	c.IndentedJSON(http.StatusNoContent, "this is partially update post")
+}
+
+func (h *handler) DeletePost(c *gin.Context) {
+	c.IndentedJSON(http.StatusNoContent, "his is delete post")
 }
